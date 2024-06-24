903a0c89c9bb46a309de101637af0cd2561690bd
minsung <menysung94@naver.com>
2024-06-24 15:09:26 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import MovieList from "./components/MovieList/MovieList";
+import Navbar from "./components/Navbar/Navbar";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <MovieList />
+    </div>
+  );
+}
