5cab7b52204c5be1de4b230458673d2edef30823
minsung <menysung94@naver.com>
2024-06-25 09:38:47 +0900

    active 클래스로 클릭시 해당평점에 밑줄 표시

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -13,13 +13,19 @@ export default function MovieList() {
     );
     const data = await response.json();
     setMovies(data.results);
-    setFilterMovies(data.results);
+    setFilterMovies(data.results); //처음엔 모든 영화를 입력
   };
   const handleFilter = (rate) => {
-    setMinRating(rate); //평점을 업데이트
-    //필터함수로 영화평점이 지정한 평점보다 높은 경우에만 남김
-    const filtered = movies.filter((movie) => movie.vote_average >= rate);
-    setFilterMovies(filtered);
+    //평점 클릭시 그 평점 이상의 영화들 보여주기
+    //평점 재클릭시 모든 영화 보여주기
+    if (minRating === rate) {
+      setMinRating(0); //평점은 0점 이상
+      setFilterMovies(movies); //처음으로 되돌림
+    } else {
+      setMinRating(rate); //평점 업데이트
+      const filtered = movies.filter((movie) => movie.vote_average >= rate);
+      setFilterMovies(filtered);
+    }
   };
 
   //시작시 한 번 영화를 불러온다
@@ -35,16 +41,35 @@ export default function MovieList() {
 
         <div className="align_center movie_list_fs">
           <ul className="align_center movie_filter">
+            {/* active : 평점 클릭했을때 밑줄 */}
             <li
-              className="movie_filter_item active"
+              className={
+                minRating === 8
+                  ? "movie_filter_item active"
+                  : "movie_filter_item"
+              }
               onClick={() => handleFilter(8)}
             >
               8+ Star
             </li>
-            <li className="movie_filter_item" onClick={() => handleFilter(7)}>
+            <li
+              className={
+                minRating === 7
+                  ? "movie_filter_item active"
+                  : "movie_filter_item"
+              }
+              onClick={() => handleFilter(7)}
+            >
               7+ Star
             </li>
-            <li className="movie_filter_item" onClick={() => handleFilter(6)}>
+            <li
+              className={
+                minRating === 6
+                  ? "movie_filter_item active"
+                  : "movie_filter_item"
+              }
+              onClick={() => handleFilter(6)}
+            >
               6+ Star
             </li>
           </ul>
